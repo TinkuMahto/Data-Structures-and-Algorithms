2970931b20b02f60587b6b78135bc9bf2ca3ff43
TinkuMahto <tinkumahto270805@gmail.com>
2026-06-25 22:27:09 +0530

    Update the solution 3737

--- 3001-4000/3737_count_subarrays_with_majority_element_I.java
+++ 3001-4000/3737_count_subarrays_with_majority_element_I.java
@@ -1,22 +1,19 @@
 class Solution {
     public int countMajoritySubarrays(int[] nums, int target) {
-        ArrayList<Integer> list = new ArrayList<>();
         int n = nums.length;
-        int subarraysCnt = 0;
-        
+        int ans = 0;
+
         for(int i=0; i<n; i++) {
-            list.clear();
             int count = 0;
             for(int j=i; j<n; j++) {
-                list.add(nums[j]);
                 if(nums[j] == target){
                     count++;
                 }
-                if(list.contains(target) && 2*count>list.size() ) {
-                    subarraysCnt++;
+                if(2*count > (j-i+1) ) {
+                    ans++;
                 }
             }
         }
-        return subarraysCnt;
+        return ans;
     }
 }
