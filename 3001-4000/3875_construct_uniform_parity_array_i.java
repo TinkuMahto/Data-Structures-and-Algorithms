27c910ba2c36bd3ca353624df28683a08fe0f92a
TinkuMahto <tinkumahto270805@gmail.com>
2026-09-03 01:05:34 +0530

    Add Leetcode Solution 3875

---
+++ 3001-4000/3875_construct_uniform_parity_array_i.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
+
+/* It is always possible to make the array all odd with at least 1 odd number since (even - odd = odd), if there are no odds the array is already all even so it is always possible, the answer is just return true */
